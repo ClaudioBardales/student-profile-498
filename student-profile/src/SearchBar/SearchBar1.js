724934db429cddb4ef70bb5d5e79a59d5fb3600b
Claudio <claudio@Claudios-Mac.local>
2022-04-03 22:22:04 -0700

    Starting Search Bar Filter Feature

--- student-profile/src/SearchBar/SearchBar1.js
+++ student-profile/src/SearchBar/SearchBar1.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const SearchBar1 = () => {
+  const [searchInput, setSearchInput] = useState('');
+
+  const searchItems = (searchValue) => {
+    setSearchInput(console.log(searchValue));
+  };
   return (
     <Test>
-      <input value="" type="text" />
+      <input type="text" onChange={(e) => searchItems(e.target.value)} />
     </Test>
   );
 };
@@ -18,6 +23,15 @@ const Test = styled.div`
   min-height: 10vh;
   position: sticky;
   top: 0;
+  input {
+    width: 100%;
+    padding: 1rem;
+    border-top: 0;
+    border-left: 0;
+    border-right: 0;
+    outline: none;
+    font-size: 30px;
+  }
 `;
 
 export default SearchBar1;
